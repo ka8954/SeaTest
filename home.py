e2a69771e1d184ace579623d9ab11f7bf5452c8a
Kaarthik Sai Charan Ayineni <93912401+ka8954@users.noreply.github.com>
2023-06-28 23:53:26 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,17 +1,94 @@
-# streamlit_app.py
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import random
+from supabase import create_client,Client
+import streamlit as slt
+import random
+import pandas as pd
 
-import streamlit as st
+url = os.environ.get("SUPABASE_URL")
+key = os.environ.get("SUPABASE_KEY")
 
-# Initialize connection.
-conn = mysql.connector.connect(host='localhost',user='root',password='dbmsproject',database='new_schema')
-cursor = conn.cursor()
-# Perform query.
-query = "SELECT * from seat"
-cursor.execute(query)
+supabase = create_client(url,key)
 
-results = cursor.fetchall()
+def main():
+    slt.set_page_config(page_title="seaTest - Instructor Interface", page_icon="✨")
+    slt.sidebar.title("")
+    options = slt.sidebar.radio('Pages', options=("Entry", "Retreival"))
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.ID} {row.USERNAME} {row.Password} {row.Sysno} {row.Setno} {row.ExamDate}:")
-conn.close()
+    def btn_click():
+        print("Option Selected")
+
+    def retreive():
+        col1, col2, col3 = slt.columns([4.2, 9, 2])
+
+        with col1:
+            slt.write("")
+
+        with col2:
+            slt.image("srm.jpg", width=300)
+            slt.write('Developed By Kaarthik Sai Charan Ayineni')
+
+        with col3:
+            slt.write("")
+        slt.header('Student Wise Seating Arrangement')
+        slt.write("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+
+        doc = slt.file_uploader("Upload Your Excel Doc Here", type=["csv"])
+        if doc is not None:
+            df = pd.read_csv(doc)
+            slt.dataframe(df)
+    def entry():
+        col1, col2, col3 = slt.columns([4.2, 9, 2])
+
+        with col1:
+            slt.write("")
+
+        with col2:
+            slt.image("srm.jpg", width=300)
+            slt.write('Developed By Kaarthik Sai Charan Ayineni')
+
+        with col3:
+            slt.write("")
+
+        slt.title('TEST SEATING ARRANGEMENT')
+        slt.subheader('DEPT OF NETWORKING AND COMMUNICATIONS')
+        slt.text(
+            'This System Allocates Seating for Students belonging to Cloud Computing, Cyber Security, IT, IOT, Networking Specializations')
+        slt.write('# Type Of Exam')
+        radio_btr = slt.selectbox(' ',
+                                  options=("Choose an One", "Internals", "University Practical", "University Theory"))
+        slt.write('# Enter the Details')
+        id = slt.text_input('USER ID', max_chars=15)
+        UNAME = slt.text_input('USERNAME', max_chars=6)
+        PWORD = slt.text_input('PASSWORD', max_chars=4)
+        labcapacity = 50
+
+        a = 1
+        x = [i for i in range(a, labcapacity + 1)]
+        random.shuffle(x)
+
+        for i in range(0, 50):
+            seat = x[i]
+            if seat % 2 == 0:
+                Setno = "SET-1"
+            else:
+                Setno = "SET-2"
+
+        exam_date = slt.date_input("SELECT THE DATE OF EXAM")
+        print(exam_date)
+
+        if slt.button("SUBMIT"):
+            d1 = supabase.table("seat").insert({"ID": id, "Username": UNAME, "Password": PWORD, "Sysno": seat, "Setno": Setno}).execute()
+            assert len(d1.data) > 0
+
+    if options == 'Entry':
+        entry()
+
+    if options == 'Retreival':
+        retreive()
+
+
+if __name__ == '__main__':
+    main()
