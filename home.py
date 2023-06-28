71d6d35e22ec3cf0bece6b04f99a0df3812c1e2a
Kaarthik Sai Charan Ayineni <93912401+ka8954@users.noreply.github.com>
2023-06-28 08:38:53 +0530

    Update home.py

--- home.py
+++ home.py
@@ -3,11 +3,15 @@
 import streamlit as st
 
 # Initialize connection.
-conn = st.experimental_connection('mysql', type='sql')
-
+conn = mysql.connector.connect(host='localhost',user='root',password='dbmsproject',database='new_schema')
+cursor = conn.cursor()
 # Perform query.
-df = conn.query('SELECT * from seat;', ttl=600)
+query = "SELECT * from seat"
+cursor.execute(query)
+
+results = cursor.fetchall()
 
 # Print results.
 for row in df.itertuples():
     st.write(f"{row.ID} {row.USERNAME} {row.Password} {row.Sysno} {row.Setno} {row.ExamDate}:")
+conn.close()
